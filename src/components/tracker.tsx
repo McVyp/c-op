7f39fd9f7131d046aecc3c136b3a838fcaf76718
McVyp <lyeclyde@gmail.com>
2024-10-31 10:23:49 +0900

    add mouse tracker for mobile

--- src/components/tracker.tsx
+++ src/components/tracker.tsx
@@ -2,13 +2,16 @@ import { useEffect } from "react";
 
 export function Tracker() {
   useEffect(() => {
-    function handleMove(event: MouseEvent) {
-      console.log(`X:${event.clientX}, Y:${event.clientY}`);
+    function handleMove(event: MouseEvent | TouchEvent) {
+      const source = "touches" in event ? event.touches[0] : event;
+      console.log(`X:${source.clientX}, Y:${source.clientY}`);
     }
     window.addEventListener("mousemove", handleMove);
+    window.addEventListener("touchmove", handleMove);
 
     return () => {
       window.removeEventListener("mousemove", handleMove);
+      window.removeEventListener("touchmove", handleMove);
     };
   }, []);
 
